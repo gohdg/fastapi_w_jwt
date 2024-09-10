68bb963d838e5dab4a68cc6be94ee4fe67d2cd32
gohdg <gohdg@naver.com>
2024-09-10 15:12:14 +0900

    crud exercise

--- main.py
+++ main.py
@@ -1,53 +1,139 @@
-from fastapi import FastAPI, Header
-from typing import Optional
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
-
+from typing import List
 
 app = FastAPI()
 
+# Endpoint
+# /books
+# /books
+# /book/{book_id}
 
-@app.get('/')
-async def read_root():
-    return {"message": "Hello World"}
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    }
+]
 
 
-@app.get('/greet')
-# query parameter
-async def greet_name(name: Optional[str] = "User", age: int = 0) -> dict:
-    return {"message": f"Hello {name}", "age": age}
-
-
-# @app.get('/greet/{name}')
-# # path parmeter & query parameter
-# async def greet_name(name: str, age:int) -> dict:
-#     return {"message": f"Hello {name}", "age": age}
-
-class BookCreateModel(BaseModel):
+class Book(BaseModel):
+    # pydantic model 정의
+    id: int
     title: str
     author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
 
-@app.post('/create_book')
-# body data가 default
-async def create_book(book_data: BookCreateModel):
-    return {
-        "title": book_data.title,
-        "author": book_data.author
-    }
+class BookUpdateModel(BaseModel):
+    # pydantic model 정의
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
 
-@app.get('/get_headers',)
-# header 데이터 접근을 위해서는 FastAPI의 Header class 필요
-# header data 접근 방법
-async def get_headers(
-    username: str = Header(None),
-    password: str = Header(None),
-    credential: str = Header(None)
-):
-    request_headers = {}
+@app.get('/books', response_model=List[Book])
+async def get_all_books():
+    return books
 
-    request_headers["username"] = username
-    request_headers["password"] = password
-    request_headers["credential"] = credential
 
-    return request_headers
+@app.post('/books', status_code=status.HTTP_201_CREATED)
+async def create_a_book(book_data: Book) -> dict:
+    new_book = book_data.model_dump()  # 모델 인스턴스를 dict로 변환
+
+    books.append(new_book)
+
+    return new_book
+
+
+@app.get('/book/{book_id}')
+async def get_book(book_id: int) -> dict:
+    for book in books:
+        if book['id'] == book_id:
+            return book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Book Not Found")
+
+
+@app.patch('/book/{book_id}')
+async def update_book(book_id: int, book_update_data: BookUpdateModel) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update_data.title
+            book["publisher"] = book_update_data.publisher
+            book["page_count"] = book_update_data.page_count
+            book["language"] = book_update_data.language
+
+            return book
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.delete('/book/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+
+            return {}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
